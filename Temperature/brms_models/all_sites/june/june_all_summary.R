dc96a314520c25147dbf4b8a838898bb1e6453e7
Jpomz <jfpomeranz@gmail.com>
2025-04-03 19:00:28 -0600

    june hurdle summary

--- Temperature/brms_models/all_sites/june/june_all_summary.R
+++ Temperature/brms_models/all_sites/june/june_all_summary.R
@@ -18,7 +18,7 @@ my_ggsave <- function(
 
 write_dir <- "Temperature/brms_models/all_sites/june"
 
-brm1 <- readRDS("Temperature/brms_models/all_sites/june/fit_rand_slopes_aug.rds")
+brm1 <- readRDS("Temperature/brms_models/all_sites/june/fit_rand_slopes_hurdle.rds")
 # plot(brm1)
 # pairs(brm1)
 
@@ -36,12 +36,12 @@ brm1$data |>
 # pp_check(brm1,
 #          type = "stat_grouped",
 #          group = "year_s")
-# #pp_check(brm1)
-# pp_check(brm1,
-#          type = "boxplot")
-# pp_check(brm1,
-#          type = "dens_overlay_grouped",
-#          group = "source")
+pp_check(brm1)
+pp_check(brm1,
+         type = "boxplot")
+pp_check(brm1,
+         type = "dens_overlay_grouped",
+         group = "source")
 # pp_check(brm1,
 #          ndraws = 100,
 #          type = "scatter_avg_grouped",
@@ -72,30 +72,28 @@ fit_data_orig |>
   distinct() |>
   arrange(source)
 
-# # add new years here ####
-# Need to fix this, if we decide to do it ####
-# year_s_vector <- brm1$data$year_s %>% unique() %>% sort()
-# # difference between standardized years
-# # max year - penultimate max year
-# year_diff <- year_s_vector[length(year_s_vector)] - year_s_vector[(length(year_s_vector)-1)] 
-# year_diff
-# new_year <- year_s_vector[length(year_s_vector)] + year_diff
-# new_year
-# 
-# new_year_df <- brm1$data |>
-#   select(source, site) |>
-#   distinct() |>
-#   mutate(year_s = c(new_year,
-#                     new_year +year_diff)) # new years 
-# 
-# fit_data <- bind_rows(fit_data, new_year_df)
+fit_data_orig |>
+  group_by(site, year) |>
+  count()
+
+fit_data_orig |> 
+  mutate(day = 15, 
+         date = make_date(year, month, day)) |>
+  ggplot(aes(x = date, 
+             y = temp_c,
+             color = source)) +
+  geom_point() +
+  facet_wrap(site~source, scales = "free_y")
 
 # epred draws ####
 # simulate types of data you might collect
 mean_year <- mean((fit_data_orig$year))
 sd_year <- sd((fit_data_orig$year))
-mean_temp <- mean((fit_data_orig$temp_c))
-sd_temp <- sd((fit_data_orig$temp_c))
+#max_temp <- max((fit_data_orig$temp_c))
+# max needs to be max of whole data
+# above is just max of fit data
+max_temp <- 20.198
+
 
 # simulate types of data you might collect
 fit_data_obs_years <- brm1$data |>
@@ -109,12 +107,11 @@ fit_data_obs_years <- brm1$data |>
 # plot of predicting new data ####
 pred_plot <- add_predicted_draws(
   newdata = fit_data_obs_years,
-  brm1,
-  allow_new_levels = TRUE) |>
+  brm1) |>
   median_qi(.prediction) |>
-  mutate(.prediction = (.prediction*sd_temp)+mean_temp,
-         .lower = (.lower*sd_temp)+mean_temp,
-         .upper = (.upper*sd_temp)+mean_temp) |>
+  mutate(.prediction = .prediction*max_temp,
+         .lower = .lower*max_temp,
+         .upper = .upper*max_temp) |>
   ggplot(aes(x = date, 
              y = .prediction,
              ymin = .lower, 
@@ -125,10 +122,10 @@ pred_plot <- add_predicted_draws(
   geom_line() +
   geom_point(data = brm1$data |>
                mutate(year = (year_s*sd_year)+mean_year,
-                      month = 8,
+                      month = 6,
                       day = 15,
                       date = make_date(year, month, day),
-                      temp = (temp_s*sd_temp)+mean_temp),
+                      temp = (temp_1*max_temp)),
              aes(x = date,
                  y = temp,
                  color = source),
@@ -146,11 +143,11 @@ pred_plot <- add_predicted_draws(
        x = "Year",
        y = "Predicted Temperature") +
   scale_fill_manual(values = c("deepskyblue", 
-                               "springgreen4",
-                               "slategrey")) +
+                               "slategrey",
+                               "springgreen4")) +
   scale_color_manual(values = c("deepskyblue", 
-                                "springgreen4",
-                                "slategrey")) 
+                                "slategrey",
+                                "springgreen4")) 
 pred_plot
 
 my_ggsave(plot = pred_plot,
@@ -165,9 +162,9 @@ fit_plot <- add_epred_draws(
   brm1,
   allow_new_levels = TRUE) |>
   median_qi(.epred) |>
-  mutate(.epred = (.epred*sd_temp)+mean_temp,
-         .lower = (.lower*sd_temp)+mean_temp,
-         .upper = (.upper*sd_temp)+mean_temp) |>
+  mutate(.epred = (.epred*max_temp),
+         .lower = (.lower*max_temp),
+         .upper = (.upper*max_temp)) |>
   ggplot(aes(x = date, 
              y = .epred,
              ymin = .lower, 
@@ -206,166 +203,6 @@ my_ggsave(plot = fit_plot,
        filename = paste(write_dir, "/fitted_temps_june.png", 
                         sep = ""))
 
-# spaghetti of fits
-fit_spag <- add_epred_draws(newdata = fit_data_obs_years,
-                            brm1,
-                            allow_new_levels = TRUE,
-                            ndraws = 100) |>
-  #mutate(.epred = (.epred*sd_temp)+mean_temp) |>
-  ggplot(aes(x = year_s, y = .epred, color = source)) +
-  geom_line(aes(y = .epred, group = paste(site, .draw)),
-            alpha = .2) +
-  facet_wrap(source~site, ncol = 3) +
-  theme_bw() +
-  labs(title = "Fitted June Temperatures",
-       subtitle = "Spaghetti Plot, 100 draws",
-       x = "Year",
-       y = "Fitted Temperature")+
-  scale_fill_manual(values = c("deepskyblue", 
-                               "springgreen4",
-                               "slategrey")) +
-  scale_color_manual(values = c("deepskyblue", 
-                                "springgreen4",
-                                "slategrey")) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  geom_abline()
-fit_spag
-
-my_ggsave(plot = fit_spag,
-       filename = paste(write_dir, "/fitted_spaghetti_june.png", 
-                        sep = ""))
-
-
-
-
-# probability of positive slopes ####
-get_variables(brm1)
-# glacier
-glac_pos <- brm1 %>%
-  spread_draws(b_year_s) %>%
-  mutate(year_pos = b_year_s > 0) %>%
-  summarize(prob_pos = mean(year_pos)) |>
-  mutate(source = "glacier")
-
-# sub ice
-sub_pos <- brm1 %>%
-  spread_draws(#b_Intercept, 
-    #b_sourcesub_ice,
-    b_year_s,
-    `b_year_s:sourcerock_glacier`) %>%
-  rename(year_sub = `b_year_s:sourcerock_glacier`) %>%
-  mutate(sub = b_year_s + year_sub, 
-         sub_pos = sub > 0) %>%
-  summarize(prob_pos = mean(sub_pos))|>
-  mutate(source = "rock_glacier")
-
-
-# snow
-snow_pos <- brm1 %>%
-  spread_draws(#b_Intercept, 
-    #b_sourcesub_ice,
-    b_year_s,
-    `b_year_s:sourcesnowfield`) %>%
-  rename(year_sub = `b_year_s:sourcesnowfield`) %>%
-  mutate(sub = b_year_s + year_sub, 
-         sub_pos = sub > 0) %>%
-  summarize(prob_pos = mean(sub_pos))|>
-  mutate(source = "snowfield")
-
-
-(prob_pos_df <- bind_rows(glac_pos, sub_pos, snow_pos))
-write_csv(prob_pos_df,
-          file = paste(write_dir, "/prob_slope_positive_june.csv", 
-                       sep = ""))
-
-
-# distribution of source slopes ####
-# halfeye plots of b_source
-b_glacier <- brm1 %>%
-  spread_draws(b_year_s) %>%
-  mutate(source = "glacier",
-         b_year_s = b_year_s * (sd_temp / sd_year))
-
-b_sub <- brm1 %>%
-  spread_draws(b_year_s,
-               `b_year_s:sourcerock_glacier`) %>%
-  rename(year_sub = `b_year_s:sourcerock_glacier`) %>%
-  mutate(b_year_s = b_year_s + year_sub,
-         b_year_s = b_year_s * (sd_temp / sd_year)) %>%
-  select(-year_sub) |>
-  mutate(source = "rock_glacier")
-
-b_snow <- brm1 %>%
-  spread_draws(b_year_s,
-               `b_year_s:sourcesnowfield`) %>%
-  rename(year_snow = `b_year_s:sourcesnowfield`) %>%
-  mutate(b_year_s = b_year_s + year_snow,
-         b_year_s = b_year_s * (sd_temp / sd_year)) %>%
-  select(-year_snow) |>
-  mutate(source = "snowfield")
-
-b_year <- bind_rows(b_glacier, b_sub, b_snow)
-
-source_slope_halfeye <- ggplot(b_year, 
-                               aes(y = source,
-                                   x = b_year_s,
-                                   fill = after_stat(x >0))) +
-  stat_halfeye() +
-  geom_vline(xintercept = 0, 
-             linetype = "dashed") +
-  scale_fill_manual(values = c("gray80", "orangered1")) +
-  theme_bw() +
-  labs(title = "Posterior Distribution of Source Slope Coefficients",
-       x = "Slope Coefficient",
-       y = "Water Source") +
-  guides(fill=guide_legend(title="Positive Slope")) +
-  annotate("text", # rock_glacier
-           x = 0.1,
-           y = 2.5,
-           label = paste("P(Positive)",
-                         round(sub_pos[1,1],2),
-                         sep = " ")) +
-  annotate("text", # Glacier
-           x = 0.1, 
-           y = 1.5,
-           label = paste("P(Positive)",
-                         round(glac_pos[1,1],2),
-                         sep = " ")) +
-  annotate("text", # snow
-           x = 0.1, 
-           y = 3.5,
-           label = paste("P(Positive)",
-                         round(snow_pos[1,1],3),
-                         sep = " "))
-
-source_slope_halfeye
-my_ggsave(plot = source_slope_halfeye,
-       filename = paste(write_dir, "/source_slope_halfeye_june.png", 
-                        sep = ""))
-
-# slope magnitude table ####
-# update these number ####
-# slope_mags <- data.frame(
-#   source = c("glacier", "rock_glacier", "snowfield"),
-#   magnitude = c(
-#     fixef(brm1)[2,1] * (sd_temp / sd_year), #glacier
-#     (fixef(brm1)[2,1]+fixef(brm1)[6,1]) * (sd_temp / sd_year), # sub_ice
-#     (fixef(brm1)[2,1]+fixef(brm1)[5,1]) * (sd_temp / sd_year) # snow
-#     
-#   ),
-#   text = "Degrees C per year",
-#   Q2.5 = c(fixef(brm1)[2,3] * (sd_temp / sd_year),
-#            (fixef(brm1)[2,3]+fixef(brm1)[6,3]) * (sd_temp / sd_year),
-#            (fixef(brm1)[2,3]+fixef(brm1)[5,3]) * (sd_temp / sd_year)),
-#   Q97.5 = c(fixef(brm1)[2,4] * (sd_temp / sd_year),
-#             (fixef(brm1)[2,4]+fixef(brm1)[6,4]) * (sd_temp / sd_year),
-#             (fixef(brm1)[2,4]+fixef(brm1)[5,4]) * (sd_temp / sd_year))
-# )
-# slope_mags
-# write_csv(slope_mags,
-#           file = paste(write_dir, "/slope_magnitudes_june.csv", 
-#                        sep = ""))
-
 
 # source slopes ####
 # source with epred draws
@@ -383,7 +220,7 @@ source_slope_epred <- add_epred_draws(
   #allow_new_levels = TRUE,
   re_formula = NA) |> # ignores random effects
   #median_qi(.epred) |>
-  mutate(.epred = (.epred*sd_temp)+mean_temp) |>
+  mutate(.epred = (.epred*max_temp)) |>
   ggplot(aes(x = date, 
              y = .epred,
              fill = source,
@@ -397,12 +234,12 @@ source_slope_epred <- add_epred_draws(
        x = "Year",
        y = "Temperature") +
   facet_wrap(~source) +
-  scale_fill_manual(values = c("deepskyblue", 
-                               "springgreen4",
-                               "slategrey")) +
-  scale_color_manual(values = c("deepskyblue", 
-                                "springgreen4",
-                                "slategrey"))
+  scale_fill_manual(values = c("deepskyblue",
+                               "slategrey",
+                               "springgreen4")) +
+  scale_color_manual(values = c("deepskyblue",
+                                "slategrey", 
+                                "springgreen4"))
   
 source_slope_epred
 my_ggsave(plot = source_slope_epred,
@@ -410,29 +247,100 @@ my_ggsave(plot = source_slope_epred,
                         sep = ""))
 
 
-# spaghetti of predictions
-# fixed effects
-source_spag <- add_epred_draws(newdata = source_data,
-                               brm1,
-                               re_formula = NA,
-                               allow_new_levels = TRUE,
-                               ndraws = 200) |>
-  mutate(.epred = (.epred*sd_temp)+mean_temp) |>
-  ggplot(aes(x = date, y = .epred, color = source)) +
-  geom_line(aes(y = .epred, group = .draw), alpha = .25) +
-  facet_wrap(~source) +
+data.frame(year_real = c(2019, 2020)) |>
+  mutate(year_s = (year_real - mean_year)/sd_year) |>
+  expand_grid(source = unique(brm1$data$source)) |>
+  add_epred_draws(brm1,
+                  re_formula = NA) |>
+  ungroup()|>
+  mutate(.epred = .epred * max_temp) |>
+  select(-.row, -.chain, -.iteration, -year_s) |>
+  pivot_wider(names_from = "year_real",
+              values_from = ".epred") |>
+  mutate(slope_new = `2020` - `2019`) |>
+  # slope_new = chnage in actual temperature right scale
+  mutate(percent_change = slope_new / `2019` *100) |>
+  group_by(source) |>
+  median_qi(percent_change)
+
+
+# prob pos
+glac_pos <- brm1 %>%
+  spread_draws(b_year_s) %>%
+  mutate(year_pos = (b_year_s) > 0) %>%
+  summarize(prob_pos = mean(year_pos)) |>
+  mutate(source = "glacier")
+
+# sub ice
+sub_pos <- brm1 %>%
+  spread_draws(#b_Intercept, 
+    #b_sourcerock_glacier,
+    b_year_s,
+    `b_year_s:sourcerock_glacier`) %>%
+  rename(year_sub = `b_year_s:sourcerock_glacier`) %>%
+  mutate(sub = b_year_s + year_sub, 
+         sub_pos = sub > 0) %>%
+  summarize(prob_pos = mean(sub_pos))|>
+  mutate(source = "rock_glacier")
+
+
+# snow
+snow_pos <- brm1 %>%
+  spread_draws(#b_Intercept, 
+    #b_sourcesnowfield,
+    b_year_s,
+    `b_year_s:sourcesnowfield`) %>%
+  rename(year_sub = `b_year_s:sourcesnowfield`) %>%
+  mutate(sub = b_year_s + year_sub, 
+         sub_pos = sub > 0) %>%
+  summarize(prob_pos = mean(sub_pos))|>
+  mutate(source = "snow_melt")
+
+
+(prob_pos_df <- bind_rows(glac_pos, sub_pos, snow_pos))
+
+
+# half eye
+data.frame(year_real = c(2019, 2020)) |>
+  mutate(year_s = (year_real - mean_year)/sd_year) |>
+  expand_grid(source = unique(temp_clean$source)) |>
+  add_epred_draws(brm1,
+                  re_formula = NA,
+                  allow_new_levels = TRUE) |>
+  ungroup()|>
+  mutate(.epred = .epred * max_temp) |>
+  select(-.row, -.chain, -.iteration, -year_s) |>
+  pivot_wider(names_from = "year_real",
+              values_from = ".epred") |>
+  mutate(slope_new = `2020` - `2019`) |>
+  # slope_new = chnage in actual temperature right scale
+  mutate(prop = slope_new / `2019` *100) |>
+  ggplot(
+    aes(y = source,
+        x = slope_new,
+        fill = after_stat(x >0))) +
+  stat_halfeye() +
+  geom_vline(xintercept = 0, 
+             linetype = "dashed") +
+  scale_fill_manual(values = c("gray80", "orangered1")) +
   theme_bw() +
-  labs(title = "Fitted June Temperatures in GTNP",
-       subtitle = "Spaghetti Plot, 200 draws",
-       x = "Year",
-       y = "Fitted Temperature")+
-  scale_fill_manual(values = c("deepskyblue", 
-                               "springgreen4",
-                               "slategrey")) +
-  scale_color_manual(values = c("deepskyblue", 
-                                "springgreen4",
-                                "slategrey"))
-source_spag
-my_ggsave(plot = source_spag,
-       filename = paste(write_dir, "/source_slope_spag_june.png", 
-                        sep = ""))
+  labs(x = "percentage change each year") +
+  annotate("text", # rock_glacier
+           x = 1,
+           y = 2.2,
+           label = paste("P(Positive)",
+                         round(sub_pos[1,1],2),
+                         sep = " ")) +
+  annotate("text", # Glacier
+           x = 1,
+           y = 1.2,
+           label = paste("P(Positive)",
+                         round(glac_pos[1,1],2),
+                         sep = " ")) +
+  annotate("text", # snow
+           x = 1,
+           y = 3.3,
+           label = paste("P(Positive)",
+                         round(snow_pos[1,1],3),
+                         sep = " ")) +
+NULL
